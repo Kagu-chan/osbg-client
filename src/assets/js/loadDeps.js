a904bae2af946ef2dd64ddf854d5e0cd88cf93d0
Kagu-chan <k.boese@kagu-chan.de>
2016-07-19 01:48:39 +0200

    Created basic electron package with directory structure and angular deps

---
+++ src/assets/js/loadDeps.js
@@ -0,0 +1,8 @@
+require('jquery');
+require("angular");
+require("angular-animate");
+require("angular-aria");
+require("angular-messages");
+require("angular-material");
+require("angular-sanitize");
+require("angular-route");
---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let win;
+
+function createWindow(){
+  win = new BrowserWindow({width:1024, height:720});
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
