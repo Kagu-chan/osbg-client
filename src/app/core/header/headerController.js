fd5ac8bd054769a8cd679f6956400cc19bb2e622
Kagu-chan <k.boese@kagu-chan.de>
2016-07-19 19:43:29 +0200

    Provide an alpha system message

--- src/app/core/header/headerController.js
+++ src/app/core/header/headerController.js
@@ -1,4 +1,6 @@
 angular.module("osbgApp")
 	.controller("HeaderController", ($scope) => {
+		$scope.isAlpha = true;
+		$scope.alphaMessage = "This is an alpha release - this means restricted functionallity!"
 		$scope.test = "This is a test";
 	});
